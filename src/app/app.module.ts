764404a043fdd2c6348fdfb7c6684f4fd25d06f2
Kenan Hacan <kh@picturepark.com>
2022-08-26 09:41:31 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxExtendedPdfViewerModule } from "ngx-extended-pdf-viewer";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ExamplePdfViewerComponent } from './example-pdf-viewer/example-pdf-viewer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ExamplePdfViewerComponent],
+  imports: [BrowserModule, NgxExtendedPdfViewerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
